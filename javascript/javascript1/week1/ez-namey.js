1efa159524efad5a10970927758b31a29dca7f64
jaimakena <jaimakena@gmail.com>
2021-05-05 11:30:22 +0200

    As I added ez-namey.js file

---
+++ javascript/javascript1/week1/ez-namey.js
@@ -0,0 +1,10 @@
+//Ez Namey
+
+const firstWordsArray = ['Easy', 'Corporate', 'Awesome', 'Tech', 'Business', 'Creative', 'Data', 'Alpha', 'Delta', 'Cloud'];
+const secondWordsArray = ['Beta', 'Infinite', 'Level', 'Collective', 'Collaboration', 'Server', 'Craft', 'Brand', 'Vital', 'Blink'];
+
+const randomNumber = Math.floor(Math.random() * 10) + 0;
+const startUpName = firstWordsArray[randomNumber] + ' ' + secondWordsArray[randomNumber];
+const numberOfCharacters = startUpName.length;
+
+console.log('The startup is: ' + startUpName + ' contains ' + numberOfCharacters + ' Characters');
