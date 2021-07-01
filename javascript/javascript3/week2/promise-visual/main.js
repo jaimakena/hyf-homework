051fbb257148b742a8683f59017b6d32a7e9bfd8
jaimakena <jaimakena@gmail.com>
2021-07-01 21:21:35 +0200

    Changed the x and y positions to reach the target

--- javascript/javascript3/week2/promise-visual/main.js
+++ javascript/javascript3/week2/promise-visual/main.js
@@ -3,23 +3,33 @@ const blueBox = document.querySelector('ul.marks li:nth-child(2)');
 const greenBox = document.querySelector('ul.marks li:nth-child(3)');
 const boxes = [redBox, blueBox, greenBox];
 
+async function moveBox(color){
+  if(color==='red'){
+    await moveElement(redBox, {x: 20-(redBox.offsetLeft), y: 300-(redBox.offsetTop)});
+  }else if(color==='blue'){
+    await moveElement(blueBox, {x: 400-(blueBox.offsetLeft), y: 300-(blueBox.offsetTop)})
+  }else{
+    await moveElement(greenBox, {x: 400-(greenBox.offsetLeft), y: 20-(greenBox.offsetTop)})
+  }
+}
 async function translateOneByOne(){
-  await moveElement(redBox, {x: 20, y: 300})
+  await moveBox('red')
     console.log(`redBox has been moved`); 
-  await moveElement(blueBox, {x: 400, y: 300})
+    await moveBox('blue');
     console.log(`blueBox has been moved`);
-  await moveElement(greenBox, {x: 400, y: 20})
+    await moveBox('green');
     console.log(`greenBox has been moved`);
 }
 translateOneByOne();
 
 async function translateAllAtOnce(){
   const response = Promise.all(
-        [moveElement(redBox, {x: 20, y: 300})],
-        [moveElement(blueBox, {x: 400, y: 300})],
-        [moveElement(greenBox, {x: 400, y: 20})]
+        [moveBox('red')],
+        [moveBox('blue')],
+        [moveBox('green')]
         )
   await response;
   console.log(`All the boxes have been moved`);
 }
 translateAllAtOnce();
+
