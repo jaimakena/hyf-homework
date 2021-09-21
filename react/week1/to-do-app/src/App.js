356c3c3554a871fa62c4f8fe3575f4e163e53a18
jaimakena <jaimakena@gmail.com>
2021-09-21 20:28:59 +0200

    Implemented the feedback

--- react/week1/to-do-app/src/App.js
+++ react/week1/to-do-app/src/App.js
@@ -1,37 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ToDoList from './ToDoList.js';
 
-const list = [
+const todos = [
   {title: "Get out of bed", timeline: "Wed Sep 13 2017"},
   {title: "Brush teeth", timeline: "Thu Sep 14 2017"},
   {title: "Eat breakfast", timeline: "Fri Sep 15 2017"},
   {title: "Play badminton", timeline: "Sat Sep 16 2017"}
-]
-
-function ToDo(props){
-  return(
-    <div>
-      <li>{props.title}, {props.timeline}</li>
-    </div>
-  )
-  }
-
-function ToDoList(props){ 
-  return(
-    <div>
-      <h1>Todo List</h1>
-      <ul>
-        {list.map(alist => <ToDo title={alist.title} timeline={alist.timeline}/>)}
-      </ul>
-    </div>
-  )
-}
-
+];
 
 function App() {
   return (
     <div className="App">
-      <ToDoList />
+      <ToDoList list={todos} />
     </div>
   );
 }
